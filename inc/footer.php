5b00cdf55d7852f235b8cc8c37eaf49d96b1bcef
AYMENsid <aymensidali8@gmail.com>
2025-01-15 15:48:10 +0100

    last

--- inc/footer.php
+++ inc/footer.php
@@ -1,78 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
-      
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <title>Footer Responsive</title>
 </head>
-<body style="margin-right:0;margin-left:0">
+<body style="">
 
+<!-- Footer -->
+<footer class="bg-light text-center text-white py-4">
+  <div class="container">
+    <!-- Title -->
+    <p class="text-black mb-4" style="font-weight: bold; direction: rtl;">قم بزيارة صفحتنا على مواقع التواصل الاجتماعي</p>
 
-
-
-
-<!-- Remove the container if you want to extend the Footer to full width. -->
-<div class="container my-5"style="margin-right:0px;margin-left:0px">
-
-  <footer class="bg-light text-center text-white " style="margin-right:0px;margin-left:0px"> 
-   <br>
-    <b div="rtl" class="text-black">قم بزيارة صفحتنا على مواقع التواصل الاجتماعي</b>
-  <!-- Grid container -->
-  <div class="container p-4 pb-0">
-    <!-- Section: Social media -->
-    <section class="mb-4">
-      <!-- Facebook -->
-      <a
-        class="btn btn-primary btn-floating m-1"
-        style="background-color: #3b5998;"
-        href="https://www.facebook.com/sweet.salty.dz2021"
-        role="button"
-        ><i class="fab fa-facebook-f"></i
-      ></a>
-
-      <!-- Twitter -->
-      <a
-        class="btn btn-primary btn-floating m-1"
-        style="background-color: #55acee;"
-        href="#!"
-        role="button"
-        ><i class="fab fa-twitter"></i
-      ></a>
-
-     
-      
-
-      <!-- Instagram -->
-      <a
-        class="btn btn-primary btn-floating m-1"
-        style="background-color: #ac2bac;"
-        href="https://instagram.com/sweet.salty.dz?igshid=YmMyMTA2M2Y="
-        role="button"
-        ><i class="fab fa-instagram"></i
-      ></a>
-
-      
-    </section>
-    <!-- Section: Social media -->
+    <!-- Social Media Icons -->
+    <div class="d-flex justify-content-center mb-4">
+      <a class="btn btn-primary btn-floating mx-2" style="background-color: #3b5998;" href="https://www.facebook.com/sweet.salty.dz2021" role="button">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a class="btn btn-primary btn-floating mx-2" style="background-color: #55acee;" href="#!" role="button">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a class="btn btn-primary btn-floating mx-2" style="background-color: #ac2bac;" href="https://instagram.com/sweet.salty.dz?igshid=YmMyMTA2M2Y=" role="button">
+        <i class="fab fa-instagram"></i>
+      </a>
+    </div>
   </div>
-  <!-- Grid container -->
 
-  <!-- Copyright -->
+  <!-- Copyright Section -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-   Sweet & Salty © 2022
-   
+    Sweet & Salty © 2022
   </div>
-  <!-- Copyright -->
 </footer>
-  
-</div>
-<!-- End of .container -->
+<!-- End Footer -->
 
-
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -2,70 +2,47 @@
 include "inc/function.php";
 $categorie = getAllcategorie();
 $produit = getAllproduct();
-if (!empty($_POST)) {// button search clicked
 
-
+if (!empty($_POST)) { // button search clicked
   $produit = searchProduit($_POST['search']);
-}
-else{
+} else {
   $produit = getAllproduct();
 }
-
-
 ?>
 
 <!doctype html>
 <html lang="en">
   <head>
-    
-      
-      
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sweet & Salty</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
-
-   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
   </head>
   <body>
+    <?php include "inc/header.php"; ?>
 
-<?php
-include "inc/header.php";
+    <div class="container mt-4">
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+    <?php foreach ($produit as $produit) : ?>
+      <div class="col">
+        <div class="card h-100">
+          <img src="images/<?= $produit['image'] ?>" class="card-img-top" alt="<?= htmlspecialchars($produit['nom']) ?>">
+          <div class="card-body d-flex flex-column">
+            <h2 class="card-title text-center" dir="rtl"><?= htmlspecialchars($produit['nom']) ?></h2>
+            <h4 class="card-text text-center"><?= htmlspecialchars($produit['prix']) ?> دج</h4>
+            <a href="produit.php?id=<?= $produit['id'] ?>" class="btn btn-primary mt-auto" style="background-color: green;">عرض المزيد</a>
+          </div>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </div>
+</div>
 
-?>
-
-  
-    <div class="row col-12" style="margin-left: 10px; margin-top: 20px;">
-
-    <?php 
-    foreach($produit as $produit ) {
-      print ' <div class="row col-12" style="margin-top: 20px;margin-left: 10px;">
-<div class="card" style="width: 18rem;">
 <br>
-  <img src="images/'.$produit['image'].'" class="card-img-top" alt="...">
-  <div class="card-body"> 
 
-   <h2 class="card-title text-center" id="nom" dir="rtl" > ' . $produit['nom'] . ' </h2>
-     <h4 class="card-text text-center">' . $produit['prix'] . ' دج </h4> 
-    <a href="produit.php?id=' . $produit['id'] . '" class="btn btn-primary " style="margin-left:50px;background-color:green;margin-top:10px"> <h5 class="white-text">عرض المزيد    </h5> </a>
-  </div> 
-</div> 
- </div>';
-    }
+    <?php include "inc/footer.php"; ?>
 
-    ?>
-             
-          
-    </div>
-   
-    <br>
-    <?php
-include "inc/footer.php";
-
-?>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
- 
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
 </html>
